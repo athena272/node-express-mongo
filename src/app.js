70a2e5fabfd9e1283984a1c36f2468f2e3532fb5
athena272 <guilhermera272@gmail.com>
2024-11-05 10:58:11 -0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -4,17 +4,23 @@ app.use(express.json())
 const books = [
     {
         id: 1,
-        título: "O Senhor dos Anéis"
+        title: "O Senhor dos Anéis"
     },
     {
         id: 2,
-        título: "O Hobbit"
+        title: "O Hobbit"
     }
 ];
 
 function searchBook(id) {
     const book = books.find(book => book.id === Number(id))
     return book
+}
+
+function searchBookID(id) {
+    const bookId = books.findIndex(books => books.id === Number(id))
+    console.log("🚀 ~ searchBookID ~ bookId:", bookId)
+    return bookId
 }
 
 app.get('/', (req, res) => {
@@ -27,10 +33,26 @@ app.get('/books', (req, res) => {
 
 app.get('/books/:id', (req, res) => {
     const book = searchBook(req.params.id)
-    res.status(200).json(book)
+    if (book) {
+        res.status(200).json(book)
+    }
+    else {
+        res.status(404).json({ "message": "Book not found" })
+    }
 })
 
 app.post('/books', (req, res) => {
     books.push(req.body)
     res.status(201).send("Add book successfully")
+})
+
+app.put('/books/:id', (req, res) => {
+    const bookID = searchBookID(req.params.id)
+    if (bookID) {
+        books[bookID].title = req.body
+        res.status(200).send("Updated book successfully")
+    }
+    else {
+        res.status(404).json({ "message": "Book not found" })
+    }
 })
