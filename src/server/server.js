a606a828db1ac56204a0dfa0fa4d92b9ee231b1b
athena272 <guilhermera272@gmail.com>
2024-11-05 09:46:20 -0300

    Update server.js

--- src/server/server.js
+++ src/server/server.js
@@ -1,6 +1,17 @@
 import http from 'http'
 
+const PORT = 3000
+
+const rotas = {
+    "/": "Curso de Node.js"
+};
+
 const server = http.createServer((req, res) => {
     res.writeHead(200, { "Content-Type": "text/plain" })
-    res.end("Curso de Node.js")
+    console.log("🚀 ~ server ~ req.url:", req.url)
+    res.end(rotas[req.url])
+})
+
+server.listen(PORT, () => {
+    console.log("Server on 🔥🔥🔥")
 })
