ba99cc9f6a2b155460afaf2a0fd0b6937c59dd42
athena272 <guilhermera272@gmail.com>
2024-11-05 13:35:10 -0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ import express from 'express'
 import connectDatabase from './config/dbConnect.js';
 const conection = await connectDatabase()
 
-conection.on('error', (error) => console.error("Something went wrong 💀", error))
+conection.on('error', (error) => console.error("Something went wrong 💀\n", error))
 conection.once('open', () => console.log("Connection established 🔥"))
 
 export const app = express()
