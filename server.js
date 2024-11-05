2ef8e271e84db4bf6f03752668fe51784ab0f205
athena272 <guilhermera272@gmail.com>
2024-11-05 15:14:27 -0300

    Create server.js

---
+++ server.js
@@ -0,0 +1,23 @@
+import 'dotenv/config'
+import { app } from './src/app.js';
+
+const PORT = 3000
+
+app.listen(PORT, () => {
+    console.log("Server on 🔥")
+})
+
+
+// import http from 'http'
+
+// const rotas = {
+//     "/": "Curso de Node.js",
+//     "/books": "Entrei na rota de livros",
+//     "/authors": "Entrei na rota de authors",
+// };
+
+// const server = http.createServer((req, res) => {
+//     res.writeHead(200, { "Content-Type": "text/plain" })
+//     console.log("🚀 ~ server ~ req.url:", req.url)
+//     res.end(rotas[req.url])
+// })
