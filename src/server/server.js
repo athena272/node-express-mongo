b3f04b93783877345b05f4ab6a27e0e98d44ab01
athena272 <guilhermera272@gmail.com>
2024-11-05 10:01:08 -0300

    Update server.js

--- src/server/server.js
+++ src/server/server.js
@@ -1,19 +1,22 @@
-import http from 'http'
+import { app } from '../app.js';
 
 const PORT = 3000
 
-const rotas = {
-    "/": "Curso de Node.js",
-    "/books": "Entrei na rota de livros",
-    "/authors": "Entrei na rota de authors",
-};
-
-const server = http.createServer((req, res) => {
-    res.writeHead(200, { "Content-Type": "text/plain" })
-    console.log("🚀 ~ server ~ req.url:", req.url)
-    res.end(rotas[req.url])
-})
-
-server.listen(PORT, () => {
+app.listen(PORT, () => {
     console.log("Server on 🔥🔥🔥")
 })
+
+
+// import http from 'http'
+
+// const rotas = {
+//     "/": "Curso de Node.js",
+//     "/books": "Entrei na rota de livros",
+//     "/authors": "Entrei na rota de authors",
+// };
+
+// const server = http.createServer((req, res) => {
+//     res.writeHead(200, { "Content-Type": "text/plain" })
+//     console.log("🚀 ~ server ~ req.url:", req.url)
+//     res.end(rotas[req.url])
+// })
