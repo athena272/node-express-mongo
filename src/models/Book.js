742cb4f8f95f706265a11634404fc8ed22105406
athena272 <guilhermera272@gmail.com>
2024-11-06 01:46:27 -0300

    Update Book.js

--- src/models/Book.js
+++ src/models/Book.js
@@ -1,11 +1,13 @@
 import mongoose from "mongoose";
+import { authorSchema } from "./Author.js";
 
 const bookSchema = new mongoose.Schema({
     id: { type: mongoose.Schema.Types.ObjectId },
     title: { type: String, required: true },
     publisher: { type: String },
     price: { type: Number, required: true },
-    number_pages: { type: Number }
+    number_pages: { type: Number },
+    author: authorSchema
 }, { versionKey: false })
 
 export const book = mongoose.model('books', bookSchema)
