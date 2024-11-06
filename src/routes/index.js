6875a2a073d2f6028b1c3178866eeab5939dcd86
athena272 <guilhermera272@gmail.com>
2024-11-06 00:25:57 -0300

    Update index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,11 @@
 import express from 'express';
 import { bookRouter } from './bookRoutes.js'
+import { authorRouter } from './authorRoutes.js';
 
 // export const app = express()
 // app.use(express.json())
 export const routes = (app) => {
     app.route("/").get((req, res) => res.status.send("Curso de Node.js"))
 
-    app.use(express.json(), bookRouter)
+    app.use(express.json(), bookRouter, authorRouter)
 }
