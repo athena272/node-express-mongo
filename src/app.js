37f273de863801f5e95960293af0022d4aa83b9b
athena272 <guilhermera272@gmail.com>
2024-11-23 22:34:02 -0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,3 +1,4 @@
+import mongoose from 'mongoose';
 import express from 'express'
 import connectDatabase from './config/dbConnect.js';
 import { routes } from './routes/index.js';
@@ -9,6 +10,17 @@ conection.once('open', () => console.log("Connection established 🔥"))
 export const app = express()
 routes(app)
 
+app.use((error, req, res, next) => {
+    if (error instanceof mongoose.Error.CastError) {
+        return res.status(400).json({
+            message: "Bad Request: Invalid data format. Please check your input."
+        });
+    }
+
+    return res.status(500).json({
+        message: "Internal Server Error: An unexpected error occurred. Please try again later."
+    });
+});
 // app.use((error, req, res, next) => {
 
 // })
