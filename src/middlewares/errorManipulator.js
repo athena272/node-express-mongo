65276145809c87afe81e03c44641aaf0ac99f91b
athena272 <guilhermera272@gmail.com>
2024-11-24 11:36:20 -0300

    Update errorManipulator.js

--- src/middlewares/errorManipulator.js
+++ src/middlewares/errorManipulator.js
@@ -1,6 +1,8 @@
 import mongoose from "mongoose";
 
 export function errorManipulator(error, req, res, next) {
+    console.log(error);  // imprime o erro para a pessoa desenvolvedoraF
+    
     if (error instanceof mongoose.Error.CastError) {
         return res.status(400).json({ message: "Bad Request: Invalid data format. Please check your input." });
     }
