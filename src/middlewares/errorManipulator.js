893e72f3c5390d35480125efb737432686d80520
athena272 <guilhermera272@gmail.com>
2025-02-17 12:33:15 -0300

    Update errorManipulator.js

--- src/middlewares/errorManipulator.js
+++ src/middlewares/errorManipulator.js
@@ -5,6 +5,10 @@ export function errorManipulator(error, req, res, next) {
     
     if (error instanceof mongoose.Error.CastError) {
         return res.status(400).json({ message: "Bad Request: Invalid data format. Please check your input." });
+    } else if (error instanceof mongoose.Error.ValidationError) {
+        const errorMessages = Object.values(error.errors).map(error => error.message).join("; ") 
+
+        return res.status(400).json({ message: `The following errors were found: ${errorMessages}`})
     }
 
     return res.status(500).json({ message: "Internal Server Error: An unexpected error occurred. Please try again later." });
