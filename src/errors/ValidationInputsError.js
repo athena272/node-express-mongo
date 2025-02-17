d1d7dca38cda66908664227ef63d6eed84ffeeff
athena272 <guilhermera272@gmail.com>
2025-02-17 18:39:57 -0300

    Update ValidationInputsError.js

--- src/errors/ValidationInputsError.js
+++ src/errors/ValidationInputsError.js
@@ -4,8 +4,6 @@ export default class ValidationInputsError extends InvalidRequest {
     constructor(error) {
         const errorMessages = Object.values(error.errors).map(error => error.message).join("; ")
 
-        super({
-            message: `The following errors were found: ${errorMessages}`
-        })
+        super(`The following errors were found: ${errorMessages}`)
     }
 }
