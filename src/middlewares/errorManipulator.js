a6e87a62afd84ecf56d6ddc55acaf565e714b1f3
athena272 <guilhermera272@gmail.com>
2025-02-17 18:03:27 -0300

    Update errorManipulator.js

--- src/middlewares/errorManipulator.js
+++ src/middlewares/errorManipulator.js
@@ -1,15 +1,19 @@
 import mongoose from "mongoose";
+import BaseError from "../errors/BaseError.js";
 
 export function errorManipulator(error, req, res, next) {
-    console.log(error);  // imprime o erro para a pessoa desenvolvedoraF
-    
+    console.log("🚀 ~ errorManipulator ~ error:", error)// imprime o erro para a pessoa desenvolvedora
+
     if (error instanceof mongoose.Error.CastError) {
         return res.status(400).json({ message: "Bad Request: Invalid data format. Please check your input." });
     } else if (error instanceof mongoose.Error.ValidationError) {
-        const errorMessages = Object.values(error.errors).map(error => error.message).join("; ") 
+        const errorMessages = Object.values(error.errors).map(error => error.message).join("; ")
 
-        return res.status(400).json({ message: `The following errors were found: ${errorMessages}`})
+        return res.status(400).json({ message: `The following errors were found: ${errorMessages}` })
     }
 
-    return res.status(500).json({ message: "Internal Server Error: An unexpected error occurred. Please try again later." });
+    new BaseError({
+        message: 'Internal Server Error: An unexpected error occurred. Please try again later.',
+        status: 500,
+    }).sendResponse(res)
 }
