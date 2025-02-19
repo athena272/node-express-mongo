4726c03684b3e0064a9438b6ed92fdf87557e53a
athena272 <guilhermera272@gmail.com>
2025-02-18 21:35:14 -0300

    Update index.js

--- src/helper/index.js
+++ src/helper/index.js
@@ -1,11 +1,21 @@
 export function procesSearch(req) {
-    const { title, publisher } = req.query
+    const { title, publisher, maxPages, minPages, authorName } = req.query
     // const regex = new RegExp(title,  "i")
     // if (title) search.title = regex
 
     const search = {}
     if (publisher) search.publisher = publisher
     if (title) search.title = { $regex: title, $options: "i" }
+
+    if (minPages || maxPages) search.number_pages = {}
+    //get = greater than or equal
+    if (minPages) search.number_pages.$gte = minPages
+    // let = less than or equal
+    if (maxPages) search.number_pages.$lte = maxPages
+
+    if (authorName)  {
+        
+    }
 
     return search
 }
