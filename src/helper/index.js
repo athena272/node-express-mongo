ddbd0922036429db3bb342b44ae0e94609800822
athena272 <guilhermera272@gmail.com>
2025-02-20 09:52:52 -0300

    Update index.js

--- src/helper/index.js
+++ src/helper/index.js
@@ -1,28 +1,28 @@
 import { author } from "../models/index.js"
 
-export async function procesSearch(req) {
-    const { title, publisher, maxPages, minPages, authorName } = req.query
+export async function processSearch(req) {
+    const { title, publisher, maxPages, minPages, authorName } = req.query;
     // const regex = new RegExp(title,  "i")
     // if (title) search.title = regex
 
-    let search = {}
-    if (publisher) search.publisher = publisher
-    if (title) search.title = { $regex: title, $options: "i" }
+    let search = {};
+    if (publisher) search.publisher = publisher;
+    if (title) search.title = { $regex: title, $options: "i" };
 
-    if (minPages || maxPages) search.number_pages = {}
-    //get = greater than or equal
-    if (minPages) search.number_pages.$gte = minPages
-    // let = less than or equal
-    if (maxPages) search.number_pages.$lte = maxPages
+    if (minPages || maxPages) search.number_pages = {};
+    if (minPages) search.number_pages.$gte = minPages;
+    if (maxPages) search.number_pages.$lte = maxPages;
 
     if (authorName) {
-        const authorFound = await author.findOne({ name: authorName })
+        const authorFound = await author.findOne({ name: authorName });
         if (authorFound) {
-            search.author = authorFound._id
+            search.author = authorFound._id;
+        } else {
+            console.log("🚀 ~ processSearch ~ No author found for:", authorName);
+            return {}; // 🚨 Retorna um objeto vazio ao invés de `null` ou `undefined`
         }
-
-        search = null
     }
 
-    return search
+    console.log("🚀 ~ processSearch ~ search:", search); // Log para depuração
+    return search;
 }
